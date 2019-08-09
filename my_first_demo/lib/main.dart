d9a9484f818c7eafbbb4a0d7dd1d0ba300eb9d2d
axun <yy@axun.com.cn>
2019-08-09 20:54:09 +0800

    基础文件

---
+++ my_first_demo/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'Home/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'yaoyun',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
+        // splashColor: Colors.white70,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
