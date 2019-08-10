d054ca6a0c37aa5a0b207bf3e17b54b57176508a
axun <yy@axun.com.cn>
2019-08-11 01:20:29 +0800

    gridView.builder的使用方法

--- lib/Home/home_third_page.dart
+++ lib/Home/home_third_page.dart
@@ -1,6 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widge/model/post.dart';
 
 class HomeThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridBuilderFunc();
+  }
+}
+
+class GridBuilderFunc extends StatelessWidget {
+  Widget _builderContents(BuildContext context, int index) {
+    return Container(
+        child: Image.network(
+      posts[index].imageUrl,
+      fit: BoxFit.cover,
+      )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      padding: EdgeInsets.all(8),
+      //GridView.count  设置主轴上面的列数   GridView.extent  设置item的宽高  不能设置列数
+      itemCount: posts.length,
+      itemBuilder: _builderContents,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3, crossAxisSpacing: 8, mainAxisSpacing: 8.0),
+    );
+  }
+}
+
+class GridViewCountFunc extends StatelessWidget {
   List<Widget> _buildContents(int count) {
     return List.generate(count, (int index) {
       //第二个参数是个回调
@@ -18,14 +49,15 @@ class HomeThirdPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView.count(
+      padding: EdgeInsets.all(8),
+      //GridView.count  设置主轴上面的列数   GridView.extent  设置item的宽高  不能设置列数
       crossAxisCount: 3,
-      crossAxisSpacing: 15,
-      mainAxisSpacing: 15,
+      crossAxisSpacing: 8.0,
+      mainAxisSpacing: 8.0,
       children: _buildContents(100),
     );
   }
 }
-
 // return Container(
 //       child: Column(
 //         mainAxisAlignment: MainAxisAlignment.center,
